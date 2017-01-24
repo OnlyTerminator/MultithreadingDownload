47e3d6e4f4a10f41cf704d980b9bbd55af0b4549
xufeng.zhang <xufeng.zhang@moji.com>
2017-01-24 17:37:56 +0800

    创建项目，简单编写下载列表界面

---
+++ app/src/main/java/com/zxf/multithreadingdownload/MainActivity.java
@@ -0,0 +1,50 @@
+package com.zxf.multithreadingdownload;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.zxf.multithreadingdownload.adapter.ListViewAdapter;
+import com.zxf.multithreadingdownload.date.FileInfo;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+    private ListView listView;
+    private ListViewAdapter adapter = null;
+    private List<FileInfo> fileList = new ArrayList<FileInfo>();
+    private String[] mUrls = new String[]{
+            "http://www.lifeofpix.com/wp-content/uploads/2016/04/life-of-pix-free-stock-wall-windows-jackiehadel.jpg",
+            "http://cncspace.newhua.com/down/MTVAlbum_Demo.rar",
+            "http://lt.newhua.com/down2/FlashAlbum_Demo.rar",
+            "http://wta.newhua.com/down/weixin6316android780.apk",
+            "http://cncspace.newhua.com/down/kingma_zp.rar"
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initDate();
+    }
+
+    private void initDate() {
+        for(int i=0 ;i<mUrls.length; i++) {
+            String fileName = mUrls[i].substring(mUrls[i].lastIndexOf("/")+1);
+            FileInfo info = new FileInfo(i,mUrls[i],fileName,0,0);
+            fileList.add(info);
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    private void initView() {
+        listView = (ListView) findViewById(R.id.lv_content);
+        adapter = new ListViewAdapter(this,fileList);
+        listView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+}
---
+++ app/src/main/java/com/zxf/multithreadingdownload/adapter/ListViewAdapter.java
@@ -0,0 +1,92 @@
+package com.zxf.multithreadingdownload.adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.zxf.multithreadingdownload.R;
+import com.zxf.multithreadingdownload.date.FileInfo;
+
+import java.util.List;
+
+/**
+ * Created by luoss on 2016/5/15.
+ */
+public class ListViewAdapter extends BaseAdapter {
+    private static String TAG = "ListViewAdapter";
+    private Context mContext;
+    private List<FileInfo> mList;
+
+    public ListViewAdapter(Context mContext, List<FileInfo> mList) {
+        this.mContext = mContext;
+        this.mList = mList;
+    }
+
+    @Override
+    public int getCount() {
+        return mList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        final FileInfo info = mList.get(position);
+        if (null == convertView) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_file_list, null);
+            holder = new ViewHolder();
+            holder.tvFileName = (TextView) convertView.findViewById(R.id.tv_file_name);
+            holder.pbProgress = (ProgressBar) convertView.findViewById(R.id.pb_progress_bar);
+            holder.btStart = (Button) convertView.findViewById(R.id.btn_start);
+            holder.btStop = (Button) convertView.findViewById(R.id.btn_stop);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.tvFileName.setText(info.getFileName());
+        holder.pbProgress.setMax(100);
+
+        holder.pbProgress.setTag(info.getUrl());
+        //start 设置开始下载点击监听
+        holder.btStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick: start download file");
+            }
+        });
+
+        //end 设置停止下载点击监听
+        holder.btStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick: stop download file");
+            }
+        });
+        //downloading 设置进度条
+        holder.pbProgress.setProgress(info.getFinished());  //设置进度条
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView tvFileName;
+        ProgressBar pbProgress;
+        Button btStart;
+        Button btStop;
+    }
+
+}
